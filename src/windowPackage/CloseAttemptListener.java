03f20b6bb7c383767e5be4eccc979b6f50de817e
JonWiggins <me@jonwiggins.org>
2020-05-02 09:43:58 -0600

    clean some logic, and add a runnable jar for ease of use

--- src/windowPackage/CloseAttemptListener.java
+++ src/windowPackage/CloseAttemptListener.java
@@ -4,46 +4,41 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 /**
- * Hold an actionListener to be called each time there is an attempt to close a window.
- *  Also called by a timer to open a new window every second.
+ * Hold an actionListener to be called each time there is an attempt to close a
+ * window. Also called by a timer to open a new window every second.
  *
  * @author Jon Wiggins
- * @version 3/4/2017
+ * @version 5/2/2020
  */
 public class CloseAttemptListener implements java.awt.event.ActionListener {
 
+    private static final int NEW_WINDOW_COUNT = 5;
+
     /**
-     * If the event that calls this method is a Timer, then one new instance of the ApplicationMain class
-     *  will be created and run, otherwise 5 will be created and run.
+     * If the event that calls this method is a Timer, then one new instance of the
+     * ApplicationMain class will be created and run, otherwise 5 will be created
+     * and run.
      *
      * @param e the triggering event
      */
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        //get the source of the object that called this method
+        // get the source of the object that called this method
         Object actionSource = e.getSource();
 
-        //if the source of the event is a timer, create and run one new instance of ApplicationMain
+        // if the source of the event is a timer, create and run one new instance of
+        // ApplicationMain
         if (actionSource instanceof Timer) {
             ApplicationMain first = new ApplicationMain();
             SwingUtilities.invokeLater(first);
         } else {
-            //otherwise create and run 5 new instances of ApplicationMain
-            ApplicationMain first = new ApplicationMain();
-            SwingUtilities.invokeLater(first);
+            // otherwise create and run new instances of ApplicationMain
+            for (int count = 0; count < NEW_WINDOW_COUNT; count++) {
+                ApplicationMain first = new ApplicationMain();
+                SwingUtilities.invokeLater(first);
+            }
 
-            ApplicationMain second = new ApplicationMain();
-            SwingUtilities.invokeLater(second);
-
-            ApplicationMain third = new ApplicationMain();
-            SwingUtilities.invokeLater(third);
-
-            ApplicationMain fourth = new ApplicationMain();
-            SwingUtilities.invokeLater(fourth);
-
-            ApplicationMain fifth = new ApplicationMain();
-            SwingUtilities.invokeLater(fifth);
         }
 
     }
